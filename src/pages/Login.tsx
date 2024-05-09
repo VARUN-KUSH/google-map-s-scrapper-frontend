9584e3f244b8fad18824a9cac13d87219ef9ff33
VARUN-KUSH <shiva92637@gmail.com>
2024-05-09 14:26:40 +0530

    home url updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -26,7 +26,7 @@ export const Login = () => {
     console.log(data);
 
     const jsonData = JSON.stringify(data);
-    const url = "https://google-map-s-scrapper-backend.vercel.app/api/v1/users/login";
+    const url = "http://localhost:8000/api/v1/users/login";
 
     axios
       .post(url, jsonData, {
